e82aee77cf41613c942b0a8d25c84581ec4a576b
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-12-30 17:47:49 -0600

    Add correct imports.

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCamera.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCamera.java
@@ -4,11 +4,13 @@
 
 package com.lwansbrough.RCTCamera;
 
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.media.CamcorderProfile;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
